2bb2f184a30c3e28b2be8e707b5bf1d903c2f894
clintsearl <clintsearl@gmail.com>
2019-03-25 12:10:02 -0600

    Changed some content

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Hello there React</h2>
+          <h2>Hello there Kimberly</h2>
           <p>Hey Kimberly, How are you doing today?</p>
-          <text>This is my website to tell the world how much I love you.</text>
+          <text>This is my website to tell the world how much I love you. <br/> 
+          I made this one with a different framework and is hosted on a different place</text>
         <ul>
           <li>Love you</li>
           <li>Clint</li>
+          <li>"     "&#x1F970;</li>
         </ul>          
         </header>
       </div>
