d3809a30398a043baae3412ea9d7e1f6e0ab8938
clintsearl <clintsearl@gmail.com>
2019-03-25 11:15:01 -0600

    first commit to host on heroku

---
+++ build/precache-manifest.23d6337068e403932cdcd96fe676072d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b24d7bc9909a451fcbe",
+    "url": "/static/css/main.6f117da0.chunk.css"
+  },
+  {
+    "revision": "1b24d7bc9909a451fcbe",
+    "url": "/static/js/main.27f8ecd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "afb837577f7babbf0a0653f07d2e7cdb",
+    "url": "/index.html"
+  }
+];
